5b36161dd84c2d64c489e8390d795af9e3f22abd
Enzo Rodrigues Oliveira <68085261+Enzo-R@users.noreply.github.com>
2022-07-12 17:01:42 -0300

    Update Postgres.ps1

--- Postgres.ps1
+++ Postgres.ps1
@@ -1,12 +1,16 @@
-﻿
+#Create Variable With path
 $INCLUDE = "C:\Program Files\PostgreSQL\10\bin\"
 
+#Add list split ;
 ($env:PATH).split(";")
 
+#Put user enviroments values in the variable
 $OLDPATH = [System.Environment]::GetEnvironmentVariable('PATH','User')
+
 
 $NEWPATH = "$INCLUDE;$OLDPATH" 
 
+#
 setx Path "$NEWPATH"
 
 
