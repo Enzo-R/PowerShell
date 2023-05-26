617a6fd614055317666f451668f628068f7f38d9
Enzo Rodrigues Olveira <en.zoo@hotmail.com>
2023-05-26 18:18:06 -0300

    Adicionando o chrome como navegador padrão

--- choco.ps1
+++ choco.ps1
@@ -9,3 +9,10 @@ choco install --confirm notepadplusplus.install
 choco install --confirm winrar
 choco install --confirm malwarebytes
 choco install --confirm greenshot
+
+$browserPath = "C:\Program Files\Google\Chrome\Application\chrome.exe"
+$registryPath = "HKCU:\Software\Microsoft\Windows\Shell\Associations\UrlAssociations\http\UserChoice"
+Set-ItemProperty -Path $registryPath -Name "ProgId" -Value "ChromeHTML"
+Set-ItemProperty -Path $registryPath -Name "Hash" -Value ([byte[]]@(0))
+Set-ItemProperty -Path $registryPath -Name "HashAlg" -Value 0
+Set-ItemProperty -Path $registryPath -Name "Application" -Value $browserPath
