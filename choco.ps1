b3883a0d8285ec1ee3aa23c20e228452c1e7d42f
Enzo Rodrigues Olveira <en.zoo@hotmail.com>
2023-05-27 13:52:01 -0300

    Arrumando as permissões de execução

--- choco.ps1
+++ choco.ps1
@@ -1,4 +1,4 @@
-Set-ExecutionPolicy Unrestricted
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope Process
 
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
@@ -10,9 +10,11 @@ choco install --confirm winrar
 choco install --confirm malwarebytes
 choco install --confirm greenshot
 
-$browserPath = "C:\Program Files\Google\Chrome\Application\chrome.exe"
-$registryPath = "HKCU:\Software\Microsoft\Windows\Shell\Associations\UrlAssociations\http\UserChoice"
-Set-ItemProperty -Path $registryPath -Name "ProgId" -Value "ChromeHTML"
-Set-ItemProperty -Path $registryPath -Name "Hash" -Value ([byte[]]@(0))
-Set-ItemProperty -Path $registryPath -Name "HashAlg" -Value 0
-Set-ItemProperty -Path $registryPath -Name "Application" -Value $browserPath
+# Definir o Google Chrome como o navegador padrão
+$chromePath = "C:\Program Files\Google\Chrome\Application\chrome.exe"  # Caminho para o executável do Google Chrome
+
+Start-Process -FilePath $chromePath -ArgumentList "--make-default-browser"
+
+# Definir o caminho do executável do Google Chrome para protocolos HTTP/HTTPS
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\Shell\Associations\UrlAssociations\http\UserChoice" -Name "ProgId" -Value "ChromeHTML"
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\Shell\Associations\UrlAssociations\https\UserChoice" -Name "ProgId" -Value "ChromeHTML"
